7946045cbc4a4f17c671a63e3d5fd1d93c90c888
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2019-11-04 22:47:32 +0700

    fix seed typo

--- db/seed/prodseed_1.sql
+++ db/seed/prodseed_1.sql
@@ -1,6 +1,6 @@
 DELETE FROM public.gold_standard WHERE wp_id=684;
 DELETE FROM public.gold_standard WHERE wp_id=2234;
-DELETE FROM public.gold_standard WHERE wp_id=5249;
+DELETE FROM public.gold_standard WHERE wp_id=5429;
 UPDATE public.gold_standard SET wrt_id=3 WHERE wp_id=809;
 UPDATE public.gold_standard SET wrt_id=1 WHERE wp_id=3968;
 INSERT INTO public.gold_standard (wp_id, wrt_id) VALUES (4976, 3);
