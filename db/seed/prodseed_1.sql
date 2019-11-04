b92ba5f671fb446b81a96b24ef77871287517f0f
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2019-11-04 22:44:44 +0700

    update seed

---
+++ db/seed/prodseed_1.sql
@@ -0,0 +1,7 @@
+DELETE FROM public.gold_standard WHERE wp_id=684;
+DELETE FROM public.gold_standard WHERE wp_id=2234;
+DELETE FROM public.gold_standard WHERE wp_id=5249;
+UPDATE public.gold_standard SET wrt_id=3 WHERE wp_id=809;
+UPDATE public.gold_standard SET wrt_id=1 WHERE wp_id=3968;
+INSERT INTO public.gold_standard (wp_id, wrt_id) VALUES (4976, 3);
+INSERT INTO public.gold_standard (wp_id, wrt_id) VALUES (5230, 1);
